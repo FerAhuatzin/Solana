e33d0d02789e217f486a156f3199f84db2537df7
ManuAlp <manuel.perezae@udlap.mx>
2023-08-26 15:37:29 -0600

    ObjectView3

--- Solana/Views/ObjectView.swift
+++ Solana/Views/ObjectView.swift
@@ -12,15 +12,40 @@ struct ObjectView: View {
     var body: some View {
         NavigationStack{
             VStack{
-                Text("Hola " + username + " soy Solana")
-                    .font(.largeTitle)
+                HStack {
+                    Text("Hola" )
+                        .font(.custom("Roboto",size: 40))
+                        
+                        
+                    Text(username)
+                        .font(.custom("Roboto",size: 40))
+                        .foregroundColor(Color(red: 0.338, green: 0.44, blue: 0.962))
+                    
+                }
+                .padding(.top, 40)
+                Text("Soy Solana")
+                    .font(.custom("Roboto",size: 30))
+               
                 
                 Text("Soy una apliación para pacientes con diagnóstico de cáncer \n \nAquí puedes encontrar estrategias de afrontamiento, información sobre el cancer, podrás interactuar con otras personas, contactar a especialistas y si necesitas ayuda siempre podrás chatear conmigo")
                     .multilineTextAlignment(.center)
-                    .padding(.top,80)
+                    .padding(.top)
+                    .padding(.trailing,40)
+                    .padding(.leading,40)
+                    .font(.custom("Roboto", size: 20))
+                
+                NavigationLink(destination: MenuView(username: username))
+                {
+                    Text("Conitnuar")
+                        .frame(width: 125.0, height: 50.0)
+                        .foregroundColor(.black)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .padding(.top,40)
+                }
                     
                 Spacer()
-                Image("Solana")
+                Image("SolanaSaludo")
                     .resizable()
                     .frame(width:200, height:200)
             }
