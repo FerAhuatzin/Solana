4a7955bba9b678c7c01b2071b33b68c442190ddd
Fer Ahuatzin <f126ag@gmail.com>
2023-08-27 17:49:06 -0600

    Especialistas

---
+++ Solana/Structs/Specialist.swift
@@ -0,0 +1,16 @@
+//
+//  Specialist.swift
+//  Solana
+//
+//  Created by Fernando Ahuatzin Gallardo on 27/08/23.
+//
+
+import Foundation
+
+struct Specialist: Hashable {
+    var name: String
+    var specialty: String
+    var city: String
+    var phoneNumber: String
+    var email: String
+}
---
+++ Solana/Structs/SpecialistRow.swift
@@ -0,0 +1,40 @@
+//
+//  SpecialistRow.swift
+//  Solana
+//
+//  Created by Fernando Ahuatzin Gallardo on 27/08/23.
+//
+
+import SwiftUI
+
+struct SpecialistRow: View {
+    var specialist: Specialist
+    var body: some View {
+        HStack {
+            Text(specialist.name)
+                .font(.custom("Roboto", size: 18))
+                .foregroundColor(Color(red: 0.338, green: 0.44, blue: 0.962))
+            Spacer()
+            VStack {
+                HStack {
+                    Spacer()
+                    Text(specialist.phoneNumber)
+                        .font(.custom("Roboto", size: 12))
+                }
+                HStack {
+                    Spacer()
+                    Text(specialist.email)
+                        .font(.custom("Roboto", size: 12))
+                }
+            }
+            
+        }
+        .padding()
+    }
+}
+
+struct SpecialistRow_Previews: PreviewProvider {
+    static var previews: some View {
+        SpecialistRow(specialist: Specialist(name: "Fernando Ahuatzin", specialty: "Oncólogo", city: "Puebla", phoneNumber: "9811184027", email: "fernando.ahuatzingo@udlap.mx"))
+    }
+}
