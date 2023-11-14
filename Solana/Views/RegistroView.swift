a16424c7d76b3f8d0818bb159802efe5aa2b5c32
LADISMTZ <ladislao.martinez.cabrera7@gmail.com>
2023-11-13 18:09:16 -0600

    Registro completo

--- Solana/Views/RegistroView.swift
+++ Solana/Views/RegistroView.swift
@@ -16,11 +16,20 @@ struct RegistroView: View {
     @State var name: String = ""
     @State var preferedName: String = ""
     @State var age: String = ""
-    @State var sex: String = "..."
-    @State var cancerType: String = ""
+    //@State var cancerType: String = ""
     @State var city: String = ""
     @State var accept: Bool = false
     @State private var isKeyboardHidden = true
+    
+    
+    @State private var selectedDate = Date()
+    
+    @State var selectedsex = -1
+    let sex = ["Masculino", "Femenino", "Prefiero no contestar"]
+    
+    @State var selectedCancerType = -1
+    @State var showBorders: [Bool] = [false,false,false,false,false,false,false,false, false, false]
+    let cancertype = ["Cáncer de mama","Cáncer de ovario","Cáncer de pulmón", "Cáncer de páncreas y leiomiosarcoma", "Leucemia", "Cáncer de hígado","Cáncer de esófago y estómago", "Melanoma", "Cáncer de colon", "Mieloma múltiple", "Cáncer de cerebro", "Cáncer de próstata", "Cáncer de cuello uterino",  "Sarcoma, cáncer de hueso y cáncer de vejiga", "Cáncer de la niñez","Cáncer de cabeza y cuello","Linfoma", "Cáncer de útero", "Cáncer de riñón","Cáncer de tiroides"]
     
     var body: some View {
         NavigationStack{
@@ -70,7 +79,7 @@ struct RegistroView: View {
                             .font(.custom("Roboto", size: 20))
                             .cornerRadius(10)
                     }
-                    .padding(.bottom,20)
+                 
 
                     
                     VStack{
@@ -84,58 +93,21 @@ struct RegistroView: View {
                             .background(Color.black.opacity(0.05))
                             .font(.custom("Roboto", size: 20))
                             .cornerRadius(10)
+                        
+                        
                         TextField("Nombre de preferencia", text: $preferedName)
                             .disableAutocorrection(true)
                             .autocapitalization(.none)
+                            .padding(8)
                             .frame(width: 300, height: 50.0)
                             .foregroundColor(.black)
                             .background(Color.black.opacity(0.05))
                             .font(.custom("Roboto", size: 20))
                             .cornerRadius(10)
                         
-                        TextField("Fecha nacimiento: dd/mm/aaaa", text: $age)
-                            .disableAutocorrection(true)
-                            .autocapitalization(.none)
-                            .padding(8)
-                            .frame(width: 300, height: 50.0)
-                            .foregroundColor(.black)
-                            .background(Color.black.opacity(0.05))
-                            .font(.custom("Roboto", size: 20))
-                            .cornerRadius(10)
-                    }
-                    
-                    HStack {
-                        Menu() {
-                            Button("Masculino"){
-                                    sex  = "Masculino"
-                                }
-                            Button("Femenino"){
-                                    sex  = "Femenino"
-                                }
-                            Button("Prefiero no contestar"){
-                                    sex = "Prefiero no contestar"
-                                }
-                        }
-                        label: {
-                            Label("Sexo:", systemImage: "chevron.down")
-                                .foregroundColor(Color(red: 0.338, green: 0.44, blue: 0.962))
-                                .font(.custom("Roboto",size: 20))
-                        }
-                        Text(sex)
-                            .font(.custom("Roboto",size: 20))
-                    }
-                    .frame(width: 300, height: 50.0)
-                    
-                    VStack {
-                        TextField("Tipo de cancer", text: $cancerType)
-                            .disableAutocorrection(true)
-                            .autocapitalization(.none)
-                            .padding(8)
-                            .frame(width: 300, height: 50.0)
-                            .foregroundColor(.black)
-                            .background(Color.black.opacity(0.05))
-                            .font(.custom("Roboto", size: 20))
-                            .cornerRadius(10)
+                        
+                        
+                        
                         TextField("Ciudad", text: $city)
                             .disableAutocorrection(true)
                             .autocapitalization(.none)
@@ -145,7 +117,54 @@ struct RegistroView: View {
                             .background(Color.black.opacity(0.05))
                             .font(.custom("Roboto", size: 20))
                             .cornerRadius(10)
+                  
+                        
+                        
+                        HStack{
+                            Spacer()
+                            DatePicker(
+                                "Fecha de nacimiento:",
+                                selection: $selectedDate,
+                                displayedComponents: [.date]
+                            )
+                            .padding(.top)
+                            .padding(.horizontal,35)
+                            
+                            Spacer()
+                        }.padding(.top,15)
                     }
+                    
+                    HStack {
+                        Picker(selection: $selectedsex, label: Text("Picker")
+                                    .foregroundColor(.black)) {
+                                        Text("Sexo")
+                                        ForEach(0..<sex.count) { index in
+                                            Text(sex[index]).tag(index)
+                                        }
+                                    }
+                                    .padding(.top)
+                                    .border(showBorders[9] ? Color.red : Color.clear)
+                    }
+                    .padding(.top,50)
+                    .frame(width: 300, height: 10.0)
+                    
+                    
+                    
+                    VStack {
+                        
+                        Picker(selection: $selectedCancerType, label: Text("Picker")
+                                    .foregroundColor(.black)) {
+                                        Text("Seleccionar tipo de cancer")
+                                        ForEach(0..<cancertype.count) { index in
+                                            Text(cancertype[index]).tag(index)
+                                        }
+                                    }
+                                    .padding(.top)
+                                    .border(showBorders[9] ? Color.red : Color.clear)
+                        
+                    }
+                    .padding(.top,40)
+                    .padding(.bottom,40)
                     
                     HStack {
                         
@@ -171,6 +190,7 @@ struct RegistroView: View {
                             
 
                     }
+                    .padding(.bottom)
                     
                     if accept {
                         NavigationLink(destination: ObjectView(username: preferedName))
